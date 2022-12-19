360c147379d809c56a77235d1b0bc78b32eaefb8
jake-shobin <jshobin12@gmail.com>
2022-12-19 10:56:32 -0500

    video-3-create-react-act

---
+++ jakefrontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">jakeamazon</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
